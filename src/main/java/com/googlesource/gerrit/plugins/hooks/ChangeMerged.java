d751897b2dfede9392ad978cd2e24367f77da9e3
David Pursehouse <dpursehouse@collab.net>
2016-06-21 10:20:49 +0900

    WiP: Adapt to latest core Gerrit API and add remaining listeners
    TODO: implement ref-update hook and support for synchronous execution
    TODO: set the change kind in RevisionCreated
    TODO: Documentation
    
    RFC: Squash this with the previous change?  I've reimplemened most of
    what was done there so I'm not sure if it makes sense to submit them
    separately.
    
    Change-Id: I96cacc61df7fd0ed31665357e89a965b1047ce75

---
+++ src/main/java/com/googlesource/gerrit/plugins/hooks/ChangeMerged.java
@@ -0,0 +1,50 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.hooks;
+
+import com.google.gerrit.extensions.common.ChangeInfo;
+import com.google.gerrit.extensions.events.ChangeMergedListener;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+class ChangeMerged implements ChangeMergedListener {
+  private final Hook hook;
+  private final HookFactory hookFactory;
+
+  @Inject
+  ChangeMerged(HookFactory hookFactory) {
+    this.hook = hookFactory.create("changeMergedHook", "change-merged");
+    this.hookFactory = hookFactory;
+  }
+
+  @Override
+  public void onChangeMerged(ChangeMergedListener.Event event) {
+    HookArgs args = hookFactory.create();
+
+    ChangeInfo c = event.getChange();
+    args.add("--change", c.id);
+    args.addUrl(c);
+    args.add("--change-owner", c.owner);
+    args.add("--project", c.project);
+    args.add("--branch", c.branch);
+    args.add("--topic", c.topic);
+    args.add("--submitter", event.getMerger());
+    args.add("--commit", c.currentRevision);
+    args.add("--newrev", event.getNewRevisionId());
+
+    hook.submit(c.project, args);
+  }
+}
---
+++ src/main/java/com/googlesource/gerrit/plugins/hooks/CommentAdded.java
@@ -0,0 +1,52 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.hooks;
+
+import com.google.gerrit.extensions.common.ChangeInfo;
+import com.google.gerrit.extensions.events.CommentAddedListener;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+class CommentAdded implements CommentAddedListener {
+  private final Hook hook;
+  private final HookFactory hookFactory;
+
+  @Inject
+  CommentAdded(HookFactory hookFactory) {
+    this.hook = hookFactory.create("commentAddedHook", "comment-added");
+    this.hookFactory = hookFactory;
+  }
+
+  @Override
+  public void onCommentAdded(CommentAddedListener.Event event) {
+    HookArgs args = hookFactory.create();
+
+    ChangeInfo c = event.getChange();
+    args.add("--change", c.id);
+    args.add("--is-draft", event.getRevision().draft);
+    args.addUrl(c);
+    args.add("--change-owner", c.owner);
+    args.add("--project", c.project);
+    args.add("--branch", c.branch);
+    args.add("--topic", c.topic);
+    args.add("--author", event.getAuthor());
+    args.add("--commit", event.getRevision().commit.commit);
+    args.add("--comment", event.getComment());
+    args.addApprovals(event.getApprovals(), event.getOldApprovals());
+
+    hook.submit(c.project, args);
+  }
+}
