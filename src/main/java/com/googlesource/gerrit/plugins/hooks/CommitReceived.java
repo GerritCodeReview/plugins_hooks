d7ca6ccb41fefe6c57e9a00824f89ded421157f0
JT Olds <hello@jtolds.com>
2017-02-15 13:55:56 -0700

    ref-update: Let hooks distinguish magic ref pushes
    Prior to this changeset, the logic about what happens with
    ref-update when someone pushes to refs/for/<branch> was broken.
    Further, the only way to tell if the push was a review to
    refs/changes/<branch> was indistinguishable from there being
    no parent.
    
    Instead, let's simply add a new argument that allows the hook
    to make the appropriate decision itself and not overload
    semantics with the previous oldrev.
    
    Change-Id: I24184d47234123450a60946af159e7c9f8375057

--- src/main/java/com/googlesource/gerrit/plugins/hooks/CommitReceived.java
+++ src/main/java/com/googlesource/gerrit/plugins/hooks/CommitReceived.java
@@ -43,22 +43,10 @@ public class CommitReceived implements CommitValidationListener {
       throws CommitValidationException {
     IdentifiedUser user = receiveEvent.user;
     String refname = receiveEvent.refName;
+    String commandRef = receiveEvent.command.getRefName();
     ObjectId old = ObjectId.zeroId();
     if (receiveEvent.commit.getParentCount() > 0) {
       old = receiveEvent.commit.getParent(0);
-    }
-
-    if (receiveEvent.command.getRefName().startsWith(REFS_CHANGES)) {
-      /*
-       * If the ref-update hook tries to distinguish behavior between pushes to
-       * refs/heads/... and refs/for/..., make sure we send it the correct
-       * refname.
-       * Also, if this is targetting refs/for/, make sure we behave the same as
-       * what a push to refs/for/ would behave; in particular, setting oldrev
-       * to 0000000000000000000000000000000000000000.
-       */
-      refname = refname.replace(R_HEADS, "refs/for/refs/heads/");
-      old = ObjectId.zeroId();
     }
 
     HookArgs args = hookFactory.createArgs();
@@ -68,6 +56,7 @@ public class CommitReceived implements CommitValidationListener {
     args.add("--uploader", user.getNameEmail());
     args.add("--oldrev", old.name());
     args.add("--newrev", receiveEvent.commit.name());
+    args.add("--cmdref", commandRef);
 
     HookResult result = hook.run(projectName, args);
     if (result != null) {
